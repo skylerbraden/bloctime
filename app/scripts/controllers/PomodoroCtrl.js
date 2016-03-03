ec7307f4b6ff27be3fc3d45aafd907fe5f76170e
Skyler Braden <skylerbraden@me.com>
2016-03-03 14:31:27 -0600

    Added timer reset and stop

--- app/scripts/controllers/PomodoroCtrl.js
+++ app/scripts/controllers/PomodoroCtrl.js
@@ -3,13 +3,6 @@
         var vm = this;
         vm.workTimer = WorkTimer;
         
-//        $scope.$watch("WorkTimer.currentTime", function() {
-//            if(WorkTimer.currentTime === 0) {
-//                alert("Your pomodoro is up! Take a break!");
-//            }
-//        });
-    }
-    
     angular
         .module('bloctime')
         .controller('PomodoroCtrl', ['WorkTimer', PomodoroCtrl]);
